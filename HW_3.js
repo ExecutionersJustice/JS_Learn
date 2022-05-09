8c19a90c39fa6d5b7b8afa9c74cd55f908c2fd17
IvanSuhaka <inkognito21blr@gmail.com>
2022-05-09 19:10:02 +0300

    Added another JS homework task and array methods

--- HW_3.js
+++ HW_3.js
@@ -1,113 +1,113 @@
-const users = [
-    {
-        firstName: 'John',
-        lastName: 'Caper',
-        phone: '7436676737634',
-        registrationDate: '12.10.2021'
-    },
-    {
-        firstName: 'Clark',
-        lastName: 'Kent',
-        phone: '4346676737634',
-        registrationDate: '16.09.2021'
-    },
-    {
-        firstName: 'Tony',
-        lastName: 'Stark',
-        phone: '7436698337634',
-        registrationDate: '11.10.2021'
-    },
-    {
-        firstName: 'Bruce',
-        lastName: 'Wayne',
-        phone: '1111176737634',
-        registrationDate: '09.10.2021'
-    },
-    {
-        firstName: 'Star',
-        lastName: 'Lord',
-        phone: '7439374737634',
-        registrationDate: '10.10.2021'
-    },
-    {
-        firstName: 'Kate',
-        lastName: 'Bishop',
-        phone: '7436693647634',
-        registrationDate: '11.10.2021'
-    },
-    {
-        firstName: 'Jerry',
-        lastName: 'James',
-        phone: '7409048737634',
-        registrationDate: '10.10.2021'
-    },
-    {
-        firstName: 'Jeremy',
-        lastName: 'Clarkson',
-        phone: '743667600289334',
-        registrationDate: '16.10.2020'
-    },
-    {
-        firstName: 'Robert',
-        lastName: 'Patrik',
-        phone: '7436676730093',
-        registrationDate: '10.10.2020'
-    },
-    {
-        firstName: 'Jonny',
-        lastName: 'Sins',
-        phone: '74923982737634',
-        registrationDate: '01.01.2021'
-    },
-    {
-        firstName: 'Andrew',
-        lastName: 'Garfield',
-        phone: '743667988344',
-        registrationDate: '09.10.2019'
-    },
-    {
-        firstName: 'Jane',
-        lastName: 'Foster',
-        phone: '74368783427634',
-        registrationDate: '09.10.2019'
-    },
-    {
-        firstName: 'Rick',
-        lastName: 'Smith',
-        phone: '700000037634',
-        registrationDate: '12.10.2021'
-    },
-];
+// const users = [
+//     {
+//         firstName: 'John',
+//         lastName: 'Caper',
+//         phone: '7436676737634',
+//         registrationDate: '12.10.2021'
+//     },
+//     {
+//         firstName: 'Clark',
+//         lastName: 'Kent',
+//         phone: '4346676737634',
+//         registrationDate: '16.09.2021'
+//     },
+//     {
+//         firstName: 'Tony',
+//         lastName: 'Stark',
+//         phone: '7436698337634',
+//         registrationDate: '11.10.2021'
+//     },
+//     {
+//         firstName: 'Bruce',
+//         lastName: 'Wayne',
+//         phone: '1111176737634',
+//         registrationDate: '09.10.2021'
+//     },
+//     {
+//         firstName: 'Star',
+//         lastName: 'Lord',
+//         phone: '7439374737634',
+//         registrationDate: '10.10.2021'
+//     },
+//     {
+//         firstName: 'Kate',
+//         lastName: 'Bishop',
+//         phone: '7436693647634',
+//         registrationDate: '11.10.2021'
+//     },
+//     {
+//         firstName: 'Jerry',
+//         lastName: 'James',
+//         phone: '7409048737634',
+//         registrationDate: '10.10.2021'
+//     },
+//     {
+//         firstName: 'Jeremy',
+//         lastName: 'Clarkson',
+//         phone: '743667600289334',
+//         registrationDate: '16.10.2020'
+//     },
+//     {
+//         firstName: 'Robert',
+//         lastName: 'Patrik',
+//         phone: '7436676730093',
+//         registrationDate: '10.10.2020'
+//     },
+//     {
+//         firstName: 'Jonny',
+//         lastName: 'Sins',
+//         phone: '74923982737634',
+//         registrationDate: '01.01.2021'
+//     },
+//     {
+//         firstName: 'Andrew',
+//         lastName: 'Garfield',
+//         phone: '743667988344',
+//         registrationDate: '09.10.2019'
+//     },
+//     {
+//         firstName: 'Jane',
+//         lastName: 'Foster',
+//         phone: '74368783427634',
+//         registrationDate: '09.10.2019'
+//     },
+//     {
+//         firstName: 'Rick',
+//         lastName: 'Smith',
+//         phone: '700000037634',
+//         registrationDate: '12.10.2021'
+//     },
+// ];
 
-function chosenOnes(users) {
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].registrationDate == "09.10.2021" || users[i].registrationDate == "10.10.2021") {
-        console.log(users[i])
-        }
-        else {
-            continue
-        }
-    }
-}
+// function chosenOnes(users) {
+//     for (let i = 0; i < users.length; i++) {
+//         if (users[i].registrationDate == "09.10.2021" || users[i].registrationDate == "10.10.2021") {
+//         console.log(users[i])
+//         }
+//         else {
+//             continue
+//         }
+//     }
+// }
 
-chosenOnes(users)
+// chosenOnes(users)
 
 // ------------------------------------------------------------------------------------------------------------
 
-const jsonData = require("./task2.json")
+// const jsonData = require("./task2.json")
 
-function uniqueRecords (users) {
-    for (let i = 0; i < users.length; i++) {
-        for (let k = i + 1; k < users.length; k++) {
-            if (JSON.stringify(users[k]) === JSON.stringify(users[i])) {
-                users.splice(k, 1)
-            }
-        }
-    }
-    return JSON.stringify(users, null, 4)
-}
+// function uniqueRecords (users) {
+//     for (let i = 0; i < users.length; i++) {
+//         for (let k = i + 1; k < users.length; k++) {
+//             if (JSON.stringify(users[k]) === JSON.stringify(users[i])) {
+//                 users.splice(k, 1)
+//             }
+//         }
+//     }
+//     return JSON.stringify(users, null, 4)
+// }
 
-console.log(uniqueRecords(jsonData))
+// console.log(uniqueRecords(jsonData))
 
 // ---------------------------------------------------------------------------------------------------------------
 
@@ -167,48 +167,81 @@ const enterprises = [
     }
 ]
 
-function companyOverview (enterprises) {
+// function companyOverview (enterprises) {
   
-  for (let i = 0; i < enterprises.length; i++) {
-      let employees_sum = 0;
-      for (let k = 0; k < enterprises[i].departments.length; k++) {
-           employees_sum += enterprises[i].departments[k].employees_count
-      }
-      if (enterprises[i].departments[0].employees_count != 0) {
-        console.log(enterprises[i].name + ` (${employees_sum} сотрудников)`)
-      } 
-      else {
-        console.log(enterprises[i].name + ` (Нет сотрудников)`)
-      }
-      for (let k = 0; k < enterprises[i].departments.length; k++) {
-        if (enterprises[i].departments[k].employees_count != 0) { 
-            console.log(`- ` + enterprises[i].departments[k].name + ` (${enterprises[i].departments[k].employees_count} сотрудников)`)
-        }
-        else {
-            console.log(`- ` + enterprises[i].departments[k].name + ` (Нет сотрудников)`)
-        }
-    }
-   } 
-}
+//   for (let i = 0; i < enterprises.length; i++) {
+//       let employees_sum = 0;
+//       for (let k = 0; k < enterprises[i].departments.length; k++) {
+//            employees_sum += enterprises[i].departments[k].employees_count
+//       }
+//       if (enterprises[i].departments[0].employees_count != 0) {
+//         console.log(enterprises[i].name + ` (${employees_sum} сотрудников)`)
+//       } 
+//       else {
+//         console.log(enterprises[i].name + ` (Нет сотрудников)`)
+//       }
+//       for (let k = 0; k < enterprises[i].departments.length; k++) {
+//         if (enterprises[i].departments[k].employees_count != 0) { 
+//             console.log(`- ` + enterprises[i].departments[k].name + ` (${enterprises[i].departments[k].employees_count} сотрудников)`)
+//         }
+//         else {
+//             console.log(`- ` + enterprises[i].departments[k].name + ` (Нет сотрудников)`)
+//         }
+//     }
+//    } 
+// }
 
-companyOverview(enterprises)
+// companyOverview(enterprises)
 
 // ----------------------------------------------------------------------------------------------------------
 
-function getEnterpriseName (value) {
-  for (let i = 0; i < enterprises.length; i++) {
-    for (let k = 0; k < enterprises[i].departments.length; k++) {
-      if (value == enterprises[i].departments[k].id) {
-        return enterprises[i].name
-      }
-      else if (value == enterprises[i].departments[k].name) {
-        return enterprises[i].name
-      }
-      else {
-        continue
-      }
+// function getEnterpriseName (value) {
+//   for (let i = 0; i < enterprises.length; i++) {
+//     for (let k = 0; k < enterprises[i].departments.length; k++) {
+//       if (value == enterprises[i].departments[k].id) {
+//         return enterprises[i].name
+//       }
+//       else if (value == enterprises[i].departments[k].name) {
+//         return enterprises[i].name
+//       }
+//       else {
+//         continue
+//       }
+//     }
+//   }
+// }
+
+// console.log(getEnterpriseName("Отдел охраны труда"))
+
+// -------------------------------------------------------------------------------------------------
+// 3. Написать функцию, которая будет добавлять предприятие. В качестве аргумента принимает название предприятия
+
+function addEnterprise (enterpriseName) {
+
+    let enterpriseId = 0
+    let departmentId = 0
+    
+    enterprises.forEach((element, index) => {
+        enterpriseId = element.id
+        element.departments.forEach((item, index) => {
+          departmentId = item.id
+        })      
+    })
+    
+    let newId = Math.max(enterpriseId, departmentId) + 1
+    
+    let newDepartment = {
+        id: newId,
+        name: enterpriseName,
+        departments: []
     }
-  }
+      enterprises.push(newDepartment)
 }
 
-console.log(getEnterpriseName("Отдел охраны труда"))
+addEnterprise("Предприятие 1")
+addEnterprise("Предприятие 4")
+addEnterprise("Предприятие 5")
+
+
+console.log(JSON.stringify(enterprises, null, 4))
+
--- test.js
+++ test.js
@@ -1,11 +1,135 @@
-const numbers = ["1", "3", "7", "4", "45", "88", "2", "11", "250", "19982", "15", "1", "22", "1", "4", "3", "55"]
+const users = [
+    {
+        firstName: 'John',
+        lastName: 'Caper',
+        phone: '7436676737634',
+        registrationDate: '12.10.2021'
+    },
+    {
+        firstName: 'Clark',
+        lastName: 'Kent',
+        phone: '4346676737634',
+        registrationDate: '16.09.2021'
+    },
+    {
+        firstName: 'Tony',
+        lastName: 'Stark',
+        phone: '7436698337634',
+        registrationDate: '11.10.2021'
+    },
+    {
+        firstName: 'Bruce',
+        lastName: 'Wayne',
+        phone: '1111176737634',
+        registrationDate: '09.10.2021'
+    },
+    {
+        firstName: 'Star',
+        lastName: 'Lord',
+        phone: '7439374737634',
+        registrationDate: '10.10.2021'
+    },
+    {
+        firstName: 'Kate',
+        lastName: 'Bishop',
+        phone: '7436693647634',
+        registrationDate: '11.10.2021'
+    },
+    {
+        firstName: 'Jerry',
+        lastName: 'James',
+        phone: '7409048737634',
+        registrationDate: '10.10.2021'
+    },
+    {
+        firstName: 'Jeremy',
+        lastName: 'Clarkson',
+        phone: '743667600289334',
+        registrationDate: '16.10.2020'
+    },
+    {
+        firstName: 'Robert',
+        lastName: 'Patrik',
+        phone: '7436676730093',
+        registrationDate: '10.10.2020'
+    },
+    {
+        firstName: 'Jonny',
+        lastName: 'Sins',
+        phone: '74923982737634',
+        registrationDate: '01.01.2021'
+    },
+    {
+        firstName: 'Andrew',
+        lastName: 'Garfield',
+        phone: '743667988344',
+        registrationDate: '09.10.2019'
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Foster',
+        phone: '74368783427634',
+        registrationDate: '09.10.2019'
+    },
+    {
+        firstName: 'Rick',
+        lastName: 'Smith',
+        phone: '700000037634',
+        registrationDate: '12.10.2021'
+    },
+];
 
-for (let i = 0; i < numbers.length; i++) {
-    for (let k = i + 1; k < numbers.length; k++) {
-        if (JSON.stringify(numbers[k]) === JSON.stringify(numbers[i])) {
-            numbers.splice(k, 1)
-        }
-    }
+// function chosenOnes(users) {
+//     let user = ""
+//     users.forEach((element, index) => {
+//         if (element.registrationDate == "09.10.2021" || element.registrationDate == "10.10.2021") {
+//             user = element
+//             console.log(user)
+//         }
+//     });
+// }
+
+// chosenOnes(users)
+
+// function chosenOnes2(users) {
+//     separateArray = []
+//     users.forEach((element, index) => {
+//         if (element.registrationDate == "09.10.2021" || element.registrationDate == "10.10.2021") {
+//             separateArray.push(element)
+//         }
+//     return separateArray
+//     });
+//     console.log(separateArray)
+// }
+
+
+// -----------------------------------------------------------------------------------------------------
+
+const jsonData = require("./task2.json")
+
+// function uniqueRecords (users) {
+//     for (let i = 0; i < users.length; i++) {
+//         for (let k = i + 1; k < users.length; k++) {
+//             if (JSON.stringify(users[k]) === JSON.stringify(users[i])) {
+//                 users.splice(k, 1)
+//             }
+//         }
+//     }
+//     return JSON.stringify(users, null, 4)
+// }
+
+// console.log(uniqueRecords(jsonData))
+
+function uniqueRecords2 (users) {
+    let separateArray2 = []
+    users.forEach((user, index) => {
+        for (let comparedUser = user + 1; comparedUser < users.length; comparedUser++)
+            if (JSON.stringify(comparedUser) === JSON.stringify(user)) {
+                users.splice(comparedUser, 1)
+            }
+    
+    })
+    return JSON.stringify(users, null, 4)
 }
 
-console.log(numbers)
+console.log(uniqueRecords2(jsonData))
